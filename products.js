81af3970e3e5e152a4d95dc59f66a0282440da2a
mf6891444@gmail.com <mf66891444@gmail.com>
2023-03-30 03:26:41 -0300

    se agrega el proyecto

---
+++ products.js
@@ -0,0 +1,51 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "harina",
+        precio :50,
+        img:
+            "https://t3.ftcdn.net/jpg/00/77/76/18/240_F_77761871_XYoWVQVOTXeIsTs6XVRxWyDqRpphy3yL.jpg",
+            cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "galletitas",
+        precio :100,
+        img:
+            "https://t4.ftcdn.net/jpg/00/61/86/73/240_F_61867303_usX7c4RO7pvbqxQATb4EdlxEpBRftvee.jpg",
+            cantidad: 1,
+
+    },
+    {
+        id: 3,
+        nombre: "cerveza",
+        precio :150,
+        img:
+            "https://t3.ftcdn.net/jpg/01/41/29/82/240_F_141298273_XQo83L2Ru1JZVX6JL7Ax1QwosNO6R4Ko.jpg",
+            cantidad: 1,
+    },    
+    {
+        id: 4,
+        nombre: "leche",
+        precio :200,
+        img:
+            "https://t3.ftcdn.net/jpg/01/09/73/50/240_F_109735031_SVasYaB6I96ZDRSr3mZ6gRvbhzjj0anI.jpg",
+            cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "verduras",
+        precio :250,
+        img:
+            "https://t3.ftcdn.net/jpg/00/01/58/06/240_F_1580633_VYebvOlRgRSzj7Ms7r6LsGmXYapi5L.jpg",
+            cantidad: 1,
+    },
+    {
+        id: 6,
+        nombre: "carne",
+        precio :300,
+        img:
+            "https://t4.ftcdn.net/jpg/02/06/90/95/240_F_206909522_pgMb7I8U39I0e11x0KWi0X98Z3A1TZ48.jpg",
+            cantidad: 1,
+    },
+];
